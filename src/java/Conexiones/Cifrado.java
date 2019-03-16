c3daf38f932c80ff9cc3984e34fef36799fc0ae0
cracellch <cracellch@gmail.com>
2019-03-16 01:27:49 -0600

    Index

--- src/java/Conexiones/Cifrado.java
+++ src/java/Conexiones/Cifrado.java
@@ -5,10 +5,48 @@
  */
 package Conexiones;
 
-/**
- *
- * @author crace
- */
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.*;
+
 public class Cifrado {
+    private static final String Algo = "AES";
+    private static final byte[] keyValue = new byte [] {'J','a','i','m','e','M','i',
+    'n','o','r','G','o','m','e','z','1',};
     
+    public static String cifrado(String Data) throws Exception{
+        Key key = generateKey(keyValue); 
+        // Este es para las subllaves del AES
+        Cipher c = Cipher.getInstance(Algo);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        //Aqui paso las subllaves para cifrar
+        byte[] cifrarvalores = c.doFinal(Data.getBytes());
+        /*La libreria Sun me sirve para poder codificar los bloques del cifrado en base 
+        64*/
+        String CifradoValor = new BASE64Encoder().encode(cifrarvalores);
+        return CifradoValor;
+    }
+    
+    public static String descifrado(String cifrado) throws Exception{
+        Key key = generateKey(keyValue); 
+        // Este es para las subllaves del AES
+        Cipher c = Cipher.getInstance(Algo);
+        c.init(Cipher.DECRYPT_MODE, key);
+        //Aqui paso las subllaves para descifrar
+        /*La libreria Sun me sirve para poder decodificar los bloques del DEScifrado en base 
+        64*/
+        
+        byte[] decoficadorvalores = new BASE64Decoder().decodeBuffer(cifrado);
+        byte[] descifradorValor = c.doFinal(decoficadorvalores);
+        
+        String descifrado = new String(descifradorValor);
+        return descifrado;
+    }
+      
+    private static Key generateKey(byte [] ll) throws Exception {
+        Key key = new SecretKeySpec(ll, Algo);
+        return key;
+    }
 }
--- src/java/Conexiones/Conexion.java
+++ src/java/Conexiones/Conexion.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package Conexiones;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author crace
- */
 public class Conexion {
+    Connection con;
     
+    public Conexion()
+        {
+        String cadena="jdbc:mysql://localhost:3306/et_bd"; 
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(cadena,"root","n0m3l0");
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            //Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            }    
+        }
+    public Connection getConexion(){
+        return con;
+    }   
 }
